a9942603496881858bafdd75ade97bdb0c68c699
Walter-Conwell <walterdanteconwelljr@gmail.com>
2023-08-13 20:55:59 -0700

    working on bootstrap navbar functionality. Will work on routes next.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import Header from "./Components/Header"
+import Navigation from "./Components/Navigation";
+// import Header from "./Components/Header"
 import About from "./Components/About"
-import Footer from "./Components/Footer"
-import Portfolio from "./Components/Portfolio"
+// import Footer from "./Components/Footer"
+// import Portfolio from "./Components/Portfolio"
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Navigation/>
+      {/* <Header/> */}
       <About/>
-      <Portfolio/>
-      <Footer/>
+      {/* <Portfolio/> */}
+      {/* <Footer/> */}
     </div>
   );
 }
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -2,23 +2,25 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Portfolio from './Portfolio';
+import Footer from './Footer';
 
 function Navigation() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#App">Walter's Portfolio</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            {/* <Nav.Link href="#Portfolio">Portfolio</Nav.Link> */}
+            {/* <Nav.Link href="#link">Link</Nav.Link> */}
+            <NavDropdown title="Explore" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#Footer">Contact</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item href="#Portfolio">Portfolio</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
                 Separated link
