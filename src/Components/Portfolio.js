b487216f41f8f5eba45d42e65e260693595ef5ee
Walter-Conwell <walterdanteconwelljr@gmail.com>
2023-08-13 19:24:39 -0700

    did some work on styling, added bootstrap and react bootstrap

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -4,15 +4,30 @@ function Portfolio() {
       <section className="container">
         <h1 className="header">Portfolio</h1>
         <p id="text">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam ex
-          sit quis placeat! Autem beatae corrupti, quibusdam magni vero,
-          officiis nemo aspernatur ut incidunt assumenda sapiente eos explicabo
-          iure nihil soluta est, error dignissimos laudantium totam adipisci
-          excepturi commodi eligendi a? Quasi illum temporibus a eveniet illo ut
-          explicabo sint?
+          Code-nundrum - codeQuizGame
+          GitHub repo Link:
+          Live deployment link:
+          <img src="./assets/images/work1.png" alt="workexperience1" />
+        </p>
+        <p id="text">
+          Code-nundrum - codeQuizGame
+          GitHub repo Link:
+          Live deployment link:
+          <img src="./assets/images/work1.png" alt="workexperience1" />
+        </p>
+        <p id="text">
+          Code-nundrum - codeQuizGame
+          GitHub repo Link:
+          Live deployment link:
+          <img src="./assets/images/work1.png" alt="workexperience1" />
+        </p>
+        <p id="text">
+          Code-nundrum - codeQuizGame
+          GitHub repo Link:
+          Live deployment link:
+          <img src="./assets/images/work1.png" alt="workexperience1" />
         </p>
         <aside id="aside">
-          <img src="./assets/images/work1.png" alt="workexperience1" />
           <img src="./assets/images/work2.png" alt="workexperience2" />
           <img src="./assets/images/work3.png" alt="workexperience3" />
         </aside>
