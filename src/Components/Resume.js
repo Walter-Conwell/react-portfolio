be46cf7f0c6d2cbc92c8781165312872d56fc6d9
Walter-Conwell <131832544+Walter-Conwell@users.noreply.github.com>
2023-08-17 23:11:27 -0600

    deploying

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -4,20 +4,20 @@ import React from "react";
 // import Header from "./Components/Header"
 // import About from "./Components/About"
 // import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 // import Footer from "./Components/Footer";
 // import Portfolio from "./Components/Portfolio";
 
 function Resume() {
-    return (
-      <div>
-        <section className="Resume">
-          <h1 className="header">Resume</h1>
-          <p id="text">work experience</p>
-          <p id="text">Links</p>
-        </section>
-        <form>lorem</form>
-      </div>
-    );
-  }
-  export default Resume;
+  return (
+    <div>
+      <section className="Resume">
+        <h1 className="header">Resume</h1>
+        <p id="text">To be added</p>
+        <p id="text">Links</p>
+      </section>
+      <form>lorem</form>
+    </div>
+  );
+}
+export default Resume;
