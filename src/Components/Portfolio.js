da0012e52c2478e0587357e70fc87034da937576
Walter-Conwell <walterdanteconwelljr@gmail.com>
2023-08-23 11:03:15 -0700

    working on project component

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,108 +1,22 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Project from "./Project";
+
 function Portfolio() {
   return (
-    <div className="Portfolio">
-      <section className="container">
-        <h1 className="header">Portfolio</h1>
-        <div id="text d-flex">
-          <p>
-            <a href="https://github.com/Walter-Conwell/codeQuizGame">repo</a>
-            <br></br>
-            <a href="https://walter-conwell.github.io/codeQuizGame/">
-              Code-Nundrum
-            </a>
-            <br></br>
-            <img
-              classname="image"
-              src="https://github.com/Walter-Conwell/codeQuizGame/raw/main/Develop/assets/images/image-1.png"
-              width="200px"
-              height="100px"
-              alt="workexperience1"
-            />
-          </p>
-          <p>
-            <a href="https://github.com/Walter-Conwell/parentSchedulingApp">
-              repo
-            </a>
-            <br></br>
-            <a href="https://murmuring-shelf-98145-47fc04f5ec66.herokuapp.com/">
-              parentSchedulingApp
-            </a>
-            <br></br>
-            <img
-              classname="image"
-              src="https://github.com/Walter-Conwell/parentSchedulingApp/raw/main/screenshot.png"
-              width="200px"
-              height="100px"
-              alt="workexperience1"
-            />
-          </p>
-          <p>
-            <a href="https://github.com/Walter-Conwell?tab=repositories">
-              repo
-            </a>
-            <br></br>
-            <a href="https://github.com/Walter-Conwell?tab=repositories">
-              Blueit
-            </a>
-            <br></br>
-            <img
-              classname="image"
-              src="https://user-images.githubusercontent.com/131832544/260524868-6ce7e916-fb5b-4169-b400-7edf55b6e4ac.png"
-              width="200px"
-              height="100px"
-              alt="workexperience1"
-            />
-          </p>
-          <p>
-            <a href="https://github.com/Walter-Conwell/noteTaker">repo</a>
-            <br></br>
-            <a href="https://note-taker-8-14-23-3695530229ce.herokuapp.com/notes">
-              noteTaker
-            </a>
-            <br></br>
-            <img
-              classname="image"
-              src="https://raw.githubusercontent.com/Walter-Conwell/noteTaker/main/public/assets/images/image.png"
-              width="200px"
-              height="100px"
-              alt="workexperience1"
-            />
-          </p>
-          <p>
-            <a href="https://github.com/Walter-Conwell/svgLogoMaker">repo</a>
-            <br></br>
-            <a href="https://drive.google.com/file/d/1qeTTFTZZj85pUeA6Of9Ln2iRCS5Z6fQ4/view">
-              svgLogoMaker
-            </a>
-            <br></br>
-            <img
-              classname="image"
-              src="https://github.com/Walter-Conwell/svgLogoMaker/assets/131832544/2d7fd6c4-4d4f-4ba6-be53-b8c3a4cade6f"
-              width="200px"
-              height="100px"
-              alt="workexperience1"
-            />
-          </p>
-          <p>
-            <a href="https://github.com/Walter-Conwell/jsPasswordGenerator">
-              repo
-            </a>
-            <br></br>
-            <a href="https://walter-conwell.github.io/jsPasswordGenerator/">
-              Password Generator
-            </a>
-            <br></br>
-            <img
-              classname="image"
-              src="https://raw.githubusercontent.com/Walter-Conwell/jsPasswordGenerator/main/assets/images/image-2.png"
-              width="200px"
-              height="100px"
-              alt="workexperience1"
-            />
-          </p>
-        </div>
-      </section>
-    </div>
+    <Navbar className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#Project">Project</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            live Link <a href="#login">Test</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
+
 export default Portfolio;
---
+++ src/Components/Project.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Project() {
+    return (
+        <div>
+            <section className="container">
+                <h1>title</h1>
+                <p>image</p>
+                <h2>repo link</h2>
+                <link>live link</link>
+                
+            </section>
+        </div>
+    );
+  }
+
+export default Project;
