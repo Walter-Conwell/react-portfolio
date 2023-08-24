25e7593e209af8203e6220c090ea7e9e30039c64
Walter-Conwell <131832544+Walter-Conwell@users.noreply.github.com>
2023-08-24 12:06:45 -0600

    back to / for paths

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -10,17 +10,17 @@ function Navigation() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="./#">Walter's Portfolio</Navbar.Brand>
+        <Navbar.Brand href="/#">Walter's Portfolio</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             {/* <Nav.Link href="#Portfolio">Portfolio</Nav.Link> */}
             {/* <Nav.Link href="#link">Link</Nav.Link> */}
             <NavDropdown title="Explore" id="basic-nav-dropdown">
-              <NavDropdown.Item href="./#/Footer">Contact</NavDropdown.Item>
-              <NavDropdown.Item href="./#/Resume">Resume</NavDropdown.Item>
+              <NavDropdown.Item href="/#/Footer">Contact</NavDropdown.Item>
+              <NavDropdown.Item href="/#/Resume">Resume</NavDropdown.Item>
               {/* below, portfolio link is working! */}
-              <NavDropdown.Item href="./#/Portfolio">Portfolio</NavDropdown.Item>
+              <NavDropdown.Item href="/#/Portfolio">Portfolio</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
                 Download Resume
