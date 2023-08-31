391be3319af7e8d0186c2411c0a9efac91fe84eb
Walter-Conwell <walterdanteconwelljr@gmail.com>
2023-08-30 22:43:51 -0700

    resume set up, and resume download functionality working as well

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -5,19 +5,24 @@ import React from "react";
 // import About from "./Components/About"
 // import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import PDF from "../Images/conwell.walter.resume.pdf";
 // import Footer from "./Components/Footer";
 // import Portfolio from "./Components/Portfolio";
 
 function Resume() {
   return (
-    <div>
-      <section className="Resume">
-        <h1 className="header">Resume</h1>
-        <p id="text">To be added</p>
-        {/* link pdf for resume display */}
-      </section>
-      <button className="btn btn-primary">Download</button>
-    </div>
+    <div className="resumeContainer">
+    <section className="Resume" width="fluid" >
+      <h1 className="header">Resume</h1>
+      <br />
+      <iframe style={{ width:"90vw", height:"40vw", }} src={PDF} title="Resume PDF"></iframe>
+    <a href={PDF} download="conwell.walter.resume.pdf">
+      {/* !button placed inside <a> makes it so that the button calls the link when clicked, executing the download! */}
+      <button className="btn btn-primary" style={{ backgroundColor: "#5bd475" }}>Download</button>
+    </a>
+    <br />
+    </section>
+  </div>
   );
 }
 export default Resume;
