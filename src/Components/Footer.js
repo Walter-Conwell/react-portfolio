d4c6e87fe4af0f5086e12f53003524625b6dc74e
Walter-Conwell <walterdanteconwelljr@gmail.com>
2023-08-31 17:21:17 -0700

    Footer.js styling fixed.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,6 +7,12 @@ function Footer() {
     <div className="Footer">
       <section id="Footer" className="Footer">
         <h1 className="header">Contact</h1>
+        <Container className="contactInfo" style={{ display: "block", width:"auto" }} >
+          <br />
+          <p className="email" style={{ fontSize: "16px", display: "inline-flex", justifyContent: "center", margin: "0", textAlign: "center" }}>
+            Email Me Here: walterdanteconwelljr@gmail.com - Or enter your info into the below fields, and I will reach out to you!
+          </p>
+
         <Container>
           <Form className="form">
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
@@ -21,10 +27,22 @@ function Footer() {
               className="mb-3"
               controlId="exampleForm.ControlTextarea1"
             >
-              <Form.Label>Phone #</Form.Label>
+              <Form.Label>Message</Form.Label>
               <Form.Control as="textarea" rows={3} />
             </Form.Group>
           </Form>
+        </Container>
+        <br />
+        {/* icon links: Linkedin, GitHub */}
+        <footer className="iconLinks" style={{fontSize: "16px", display: "flex", alignItems: "center", justifyContent: "center", margin: "auto" }}>
+          
+          <a style={{ marginRight: "10px" }} href={"https://www.linkedin.com/in/walter-conwell-575586159/"} >
+          <img className="Icon" style={{ height: "40px", width: "40px" }} src="https://th.bing.com/th/id/OIP.EIJEIp5je4VfUmBcuf1ivAHaHa?w=199&h=199&c=7&r=0&o=5&pid=1.7" alt="LinkedinProfile"></img>
+          </a>
+          <a style={{ marginLeft: "10px" }} href={"https://github.com/Walter-Conwell"} >
+          <img className="Icon" style={{ height: "40px", width: "40px" }} src="https://th.bing.com/th?id=OIP.ckeUFk-yid0vfWnd56w7wAHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" alt="LinkedinProfile"></img>
+          </a>
+          </footer>
         </Container>
       </section>
     </div>
